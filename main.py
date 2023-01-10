22cfd960e5937b39ba81bb0d51b3bb092931bea1
tapok <trump6009@gmail.com>
2023-01-11 00:35:31 +0300

    3-Добавил интервал между сообщениями

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 import openai
 import telebot
+import time
 from settings import OPENAI_API_KEY, TELEGRAM_API_KEY
 
 openai.api_key = OPENAI_API_KEY
 
 bot = telebot.TeleBot(TELEGRAM_API_KEY)
 
+last_message_time = {}
+
 
 @bot.message_handler(commands=['start', 'restart'])
 def start(prompt):
     message = f'Привет, я готов ответить на твои запросы, {prompt.from_user.first_name}'
     bot.send_message(prompt.chat.id, message)
+
+
+def check_time_limit(user_id):
+    # Получаем текущее время
+    current_time = time.time()
+    # Проверяем, есть ли пользователь в словаре
+    if user_id in last_message_time:
+        # Получаем время последнего сообщения
+        last_message = last_message_time[user_id]
+        # Проверяем, прошло ли больше минуты с момента последнего сообщения
+        if current_time - last_message > 30:
+            # Обновляем время последнего сообщения
+            last_message_time[user_id] = current_time
+            return True
+        else:
+            return False
+    else:
+        # Добавляем пользователя в словарь
+        last_message_time[user_id] = current_time
+        return True
 
 
 @bot.message_handler()
@@ -23,10 +46,10 @@ def handle_message(prompt):
         top_p=1.0,
         frequency_penalty=0.5,
         presence_penalty=0.0
-    )
+    ) if check_time_limit(prompt.chat.id) else False
 
-    message = response.choices[0].text if response else 'Слишком много запросов, не успеваю, повторите через минуту!'
+    message = response.choices[0].text if response else 'Интервал между запросами не должен быть менее 30 секунд. Повторите позже!'
     bot.send_message(chat_id=prompt.chat.id, text=message)
 
 
-bot.polling()
+bot.infinity_polling()
