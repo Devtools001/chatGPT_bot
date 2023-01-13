d9fe643f2e76f664572e036af1d0f618b63e8d49
Devtools001 <122378121+Devtools001@users.noreply.github.com>
2023-01-13 14:17:50 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ import dotenv
 
 dotenv.load_dotenv('.env')
 
-OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
-TELEGRAM_API_KEY = os.environ['TELEGRAM_API_KEY']
+OPENAI_API_KEY = os.environ['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']
+TELEGRAM_API_KEY = os.environ['5907879583:AAEZUvNNGjLzbQNmxoNHQ8Q1xUKL8lF-iso']
 
